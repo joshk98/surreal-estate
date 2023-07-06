e21de9fc49baa97b03a140c109c64befc2fc6f3e
joshkkuhn@gmail.com <joshkkuhn@gmail.com>
2023-07-06 10:42:42 +0100

    Created Alert component, tests, and styles files

---
+++ src/components/Alert.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "../styles/alert.css";
+
+const Alert = () => {
+  return <div>Alert</div>;
+};
+
+export default Alert;
---
+++ src/tests/Alert.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Alert from "../components/Alert";
+
+describe("Alert", () => {
+  it("renders correctly", () => {
+    const { asFragment } = render(<Alert />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
