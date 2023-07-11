58c8b65e52edd2cd2645cc3c9463eee27c838bc7
joshkkuhn@gmail.com <joshkkuhn@gmail.com>
2023-07-11 21:05:42 +0100

    Added search functionality

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import qs from "qs";
 
 import "../styles/sidebar.css";
@@ -9,14 +9,40 @@ const buildQueryString = (operation, valueObj) => {
   const currentQueryParams = qs.parse(search, { ignoreQueryPrefix: true });
   const newQueryParams = {
     ...currentQueryParams,
-    [operation]: JSON.stringify(valueObj),
+    [operation]: JSON.stringify({
+      ...JSON.parse(currentQueryParams[operation] || "{}"),
+      ...valueObj,
+    }),
   };
   return qs.stringify(newQueryParams, { addQueryPrefix: true, encode: false });
 };
 
 const SideBar = () => {
+  const [query, setQuery] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    const newQueryString = buildQueryString("query", {
+      title: { $regex: query },
+    });
+    navigate(newQueryString);
+  };
+
   return (
     <div className="side-bar__links">
+      <form className="search-form" onSubmit={handleSearch}>
+        <input
+          className="search-input"
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
       <Link to="/">Show All</Link>
       <Link to={buildQueryString("query", { city: "Manchester" })}>
         Manchester
